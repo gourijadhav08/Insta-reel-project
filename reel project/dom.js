49e75cec42d25d622b33a4056dc09fb9ec6567b5
gouri <gourivamanjadhav06@gmail.com>
2026-03-12 20:45:22 +0530

    Add files via upload

---
+++ reel project/dom.js
@@ -0,0 +1,9 @@
+var button = document.querySelector('button')
+
+button.addEventListener('click',function(){
+               var h1 = document.createElement('div')
+               h1.innerHTML = 'gouri'
+
+    console.log(h1);
+})
+
